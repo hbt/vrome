e75e8a70ccd23a57ebf68f34c79114b405c56661
hbt <hassenbentanfous@gmail.com>
2011-02-14 21:03:24 -0500

    fixed issue with elements keeping the focus after being clicked

--- src/frontend/modules/hint.js
+++ src/frontend/modules/hint.js
@@ -254,22 +254,23 @@ var Hint = (function() {
 
       if (old_target) elem.setAttribute('target',old_target);
 
-    } else if (elem.onclick || (tag_name == 'input' && (type == 'submit' || type == 'button' || type == 'reset' || type == 'radio' || type == 'checkbox')) || tag_name == 'button') {
+    }
+    else if(elem.onclick && type != 'text') {
+        clickElement(elem);
+    }
+    else if ((tag_name == 'input' && (type == 'submit' || type == 'button' || type == 'reset' || type == 'radio' || type == 'checkbox')) || tag_name == 'button') {
       clickElement(elem);
-
-    } else if (tag_name == 'input' || tag_name == 'textarea') {
+    }
+    else if (tag_name == 'input' || tag_name == 'textarea') {
       try {
         elem.focus();
         elem.setSelectionRange(elem.value.length, elem.value.length);
       } catch(e) {
         clickElement(elem); // some website don't use standard submit input.
       }
-
     } else if (tag_name == 'select') {
       elem.focus();
     }
-
-    elem.focus();
 
     if(!repeat) {
       remove();
