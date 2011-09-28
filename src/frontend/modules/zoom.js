32eb7a963b5f0fc31b99de05b4000b02174ec9fe
hbt <hassenbentanfous@gmail.com>
2011-09-28 18:41:53 -0400

    fixed zoom to use its own key. Less JSON parsing

--- src/frontend/modules/zoom.js
+++ src/frontend/modules/zoom.js
@@ -1,45 +1,70 @@
 var Zoom = (function() {
-  var levels = ['30%', '50%', '67%', '80%', '90%', '100%', '110%', '120%', '133%', '150%', '170%', '200%', '240%', '300%'];
-  var default_index = levels.indexOf('100%');
+    var levels = ['30%', '50%', '67%', '80%', '90%', '100%', '110%', '120%', '133%', '150%', '170%', '200%', '240%', '300%'];
+    var default_index = levels.indexOf('100%');
 
-  function currentLevel() {
-    for (var i in levels) {
-      if (levels[i] == (document.body.style.zoom || '100%')) {
-        return Number(i);
-      }
+    function currentLevel() {
+        for (var i in levels) {
+            if (levels[i] == (document.body.style.zoom || '100%')) {
+                return Number(i);
+            }
+        }
     }
-  }
 
-  function setZoom(/*Number*/ count,/*Boolean*/ keepCurrentPage) {
-		var index = count ? (currentLevel() + (times() * Number(count))) : default_index
-    // index should >= 0 && < levels.length
-    index = Math.min(levels.length - 1, Math.max(0,index));
+    function setZoom(/*Number*/ count,/*Boolean*/ keepCurrentPage) {
+        var index = count ? (currentLevel() + (times() * Number(count))) : default_index
+        // index should >= 0 && < levels.length
+        index = Math.min(levels.length - 1, Math.max(0,index));
 
-    Settings.add({zoom_level : index - default_index});
-    var topPercent = scrollY / document.height;
+        Settings.setValue(getKey(), index - default_index);
+        var topPercent = scrollY / document.height;
 
-    document.body.style.zoom  = levels[index];
-    if(keepCurrentPage) scrollTo(0,topPercent * document.height);
-  }
+        document.body.style.zoom  = levels[index];
+        if(keepCurrentPage) scrollTo(0,topPercent * document.height);
+    }
 
+    function getKey() {
+        return 'vrome_zoom_level_' + location.hostname;
+    }
 
-  // API
+    return {
+        setZoom    : setZoom,
+        'in'       : function() {
+            setZoom( 1);
+        },
+        out        : function() {
+            setZoom(-1);
+        },
+        more       : function() {
+            setZoom( 3);
+        },
+        reduce     : function() {
+            setZoom(-3);
+        },
+        reset      : function() {
+            setZoom(  );
+        },
 
-	return {
-		setZoom    : setZoom,
-		'in'       : function() { setZoom( 1); },
-		out        : function() { setZoom(-1); },
-		more       : function() { setZoom( 3); },
-		reduce     : function() { setZoom(-3); },
-		reset      : function() { setZoom(  ); },
+        current_in     : function() {
+            setZoom( 1, true);
+        },
+        current_out    : function() {
+            setZoom(-1, true);
+        },
+        current_more   : function() {
+            setZoom( 3, true);
+        },
+        current_reduce : function() {
+            setZoom(-3, true);
+        },
+        current_reset  : function() {
+            setZoom( 0, true);
+        },
 
-		current_in     : function() { setZoom( 1, true); },
-		current_out    : function() { setZoom(-1, true); },
-		current_more   : function() { setZoom( 3, true); },
-		current_reduce : function() { setZoom(-3, true); },
-		current_reset  : function() { setZoom( 0, true); },
-
-		current    : function() { return (parseInt(levels[currentLevel()]) / 100); },
-		init       : function() { Zoom.setZoom( Settings.get('zoom_level')); }
-	}
+        current    : function() {
+            return (parseInt(levels[currentLevel()]) / 100);
+        },
+        init       : function() {
+            Zoom.setZoom( Settings.getValue(getKey()));
+        }
+    }
 })()
--- src/shared/settings.js
+++ src/shared/settings.js
@@ -1,41 +1,61 @@
 var Settings = (function() {
- var key = '__vrome_setting';
+    var key = '__vrome_setting';
 
- function extend(to,from) {
-   if (!to) to = {};
-   for(var p in from) to[p] = from[p];
-   return to;
- }
+    function extend(to,from) {
+        if (!to) to = {};
+        for(var p in from) to[p] = from[p];
+        return to;
+    }
 
- function currentSetting(){
-   return JSON.parse(localStorage[key] || "{}");
- }
+    function currentSetting(){
+        return JSON.parse(localStorage[key] || "{}");
+    }
 
- function add(object) {
-   if (object instanceof Object) {
-     object            = extend( currentSetting(), object);
-     localStorage[key] = JSON.stringify(object);
-     return object;
-   } else {
-     var name  = arguments[0];
-     var value = arguments[1];
-     var old_object = object = currentSetting();
-     var name = name.split('.');
-     while (name.length > 1) { object = object[name.shift()]; }
-     object[name.shift()] = value;
-     localStorage[key] = JSON.stringify(old_object);
-     return old_object;
-   }
- }
+    function add(object) {
+        if (object instanceof Object) {
+            object            = extend( currentSetting(), object);
+            localStorage[key] = JSON.stringify(object);
+            return object;
+        } else {
+            var name  = arguments[0];
+            var value = arguments[1];
+            var old_object = object = currentSetting();
+            var name = name.split('.');
+            while (name.length > 1) {
+                object = object[name.shift()];
+            }
+            object[name.shift()] = value;
+            localStorage[key] = JSON.stringify(old_object);
+            return old_object;
+        }
+    }
 
- function get(names) {
-   var object = currentSetting();
-   if(!names) return object;
+    function get(names) {
+        var object = currentSetting();
+        if(!names) return object;
 
-   var names = names.split('.');
-   while (object && names[0]) { object = object[names.shift()]; }
-   return (typeof object == 'undefined') ? '' : object;
- }
+        var names = names.split('.');
+        while (object && names[0]) {
+            object = object[names.shift()];
+        }
+        return (typeof object == 'undefined') ? '' : object;
+    }
 
- return { add : add, get :get }
+    function setValue(key, value) {
+        localStorage[key] = value;
+    }
+
+    function getValue(key, defaultValue) {
+        if(localStorage[key] === undefined) {
+            return defaultValue;
+        }
+        return localStorage[key];
+    }
+
+    return {
+        add : add,
+        get :get,
+        setValue: setValue,
+        getValue: getValue
+    }
 })();
