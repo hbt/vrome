ed28cf74afb5b4644c231b6a33acc75de28095a5
hbt <hassenbentanfous@gmail.com>
2011-11-24 16:54:23 -0500

    switched vromerc to js

--- .vromerc.js
+++ .vromerc.js
@@ -57,6 +57,7 @@ function backendExec() {
     Marks.setQuickMark('cmsf', 'http://localhost:7072/frontend_debug.php');
     Marks.setQuickMark('cucp', '::javascript::CustomMark.convertURLCTMSProd();');
     Marks.setQuickMark('pfctms', 'https://secure.empowerhealthresearch.ca/secure/form_editor.php');
+    Marks.setQuickMark('m', 'https://pmrobot.com/ims/next');
 }
 
 var CustomMark = {
